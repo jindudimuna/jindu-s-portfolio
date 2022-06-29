598bd9eef6cdd3d3ae9ef33eb118536de8564fbb
jindudimuna <danieldimuna@gmail.com>
2022-06-29 17:31:56 +0100

    packages and animation

---
+++ dist/index.c36f364e.js
@@ -0,0 +1,5 @@
+document.querySelector(".second-button").addEventListener("click", function() {
+    document.querySelector(".animated-icon2").classList.toggle("open");
+});
+
+//# sourceMappingURL=index.c36f364e.js.map
